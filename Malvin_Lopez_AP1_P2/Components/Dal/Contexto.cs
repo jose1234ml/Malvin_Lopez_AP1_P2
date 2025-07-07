ae9eecf5d1c20805c8f05dd67bb0eb76eabc7673
macbookair <macbookair@MACBOOKAIRDCC0>
2025-07-07 19:18:17 -0400

    Crea contexto

---
+++ Malvin_Lopez_AP1_P2/Components/Dal/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Malvin_Lopez_AP1_P2.Components.Dal
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Registro> Registros { get; set; } = null!;
+
+       
+    }
+}
